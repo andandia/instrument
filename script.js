8c6d7a9f3b2e49a2b06ce4cee66d0dcc21c5773a
e <a>
2025-08-17 21:40:31 +0900

    微修正

--- script.js
+++ script.js
@@ -7,8 +7,18 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     const keys = [
-        { note: 'C', type: 'white' }, { note: 'C#', type: 'black' }, { note: 'D', type: 'white' }, { note: 'D#', type: 'black' }, { note: 'E', type: 'white' },
-        { note: 'F', type: 'white' }, { note: 'F#', type: 'black' }, { note: 'G', type: 'white' }, { note: 'G#', type: 'black' }, { note: 'A', type: 'white' }, { note: 'A#', type: 'black' }, { note: 'B': 'white' }
+        { note: 'C', type: 'white' },
+        { note: 'C#', type: 'black' },
+        { note: 'D', type: 'white' },
+        { note: 'D#', type: 'black' },
+        { note: 'E', type: 'white' },
+        { note: 'F', type: 'white' },
+        { note: 'F#', type: 'black' },
+        { note: 'G', type: 'white' },
+        { note: 'G#', type: 'black' },
+        { note: 'A', type: 'white' },
+        { note: 'A#', type: 'black' },
+        { note: 'B', type: 'white' }
     ];
 
     let synthesizer;
